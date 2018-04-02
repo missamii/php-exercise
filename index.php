517e606881de99cda7297aa6571f3c9e5babd903
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2018-04-02 12:55:31 -0400

    php exercise

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello World</title>
+  </head>
+  <body>
+    <!-- this is just for practices, it's good to go back to basics sometimes. :) -->
+    <?php
+
+
+     ?>
+
+  </body>
+</html>
